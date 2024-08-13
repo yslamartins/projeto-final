ce3fc7c6bb2e360d0c7e654ff088c4e4f986e53e
Ezequias Bernardo <62806474+Ez3qu14s@users.noreply.github.com>
2024-08-13 15:13:24 -0300

    feat: models de categories finalizado

--- src/models/CategoriasModel.js
+++ src/models/CategoriasModel.js
@@ -1,9 +1,9 @@
 const connection = require('./Connection');
 
 async function getAllCategories() {
-  const categories = await connection.query('SELECT * FROM categories');
+  const getCategories = await connection.query('SELECT * FROM categories');
 
-  return categories.rows;
+  return getCategories.rows;
 }
 
 async function getCategoryById(id) {
@@ -14,6 +14,46 @@ async function getCategoryById(id) {
   return category.rows[0];
 }
 
+async function insertCategoryModel(name, slug, useMenu) {
+  await connection.query(`
+    INSERT INTO categories (name, slug, use_in_menu)
+    VALUES('${name}', '${slug}', ${useMenu})
+  `);
+
+  return;
+}
+
+async function updateCategoryPropertyModel(id, property, newValue) {
+  await connection.query(`
+    UPDATE categories
+    SET ${property} = ${newValue}
+    WHERE id = ${id}
+  `);
+
+  return;
+}
+
+async function updateEveryPropertyCategoryModel(id, name, slug, useMenu) {
+  await connection.query(`
+    UPDATE categories
+    SET name = ${name}, slug = ${slug}, use_in_menu = ${useMenu}
+    WHERE id = ${id}
+  `);
+
+  return;
+}
+
+async function deleteCategoryModel(id) {
+  await connection.query(`DELETE FROM categories WHERE id = ${id}`);
+
+  return;
+}
+
 module.exports = {
   getAllCategories,
+  getCategoryById,
+  insertCategoryModel,
+  updateCategoryPropertyModel,
+  updateEveryPropertyCategoryModel,
+  deleteCategoryModel,
 };
