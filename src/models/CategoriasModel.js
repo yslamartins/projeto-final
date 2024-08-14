753203bcdd650af05526cd5945f24e40e47da02d
Ezequias Bernardo <62806474+Ez3qu14s@users.noreply.github.com>
2024-08-14 15:54:48 -0300

    feat: continuacao dos models

--- src/models/CategoriasModel.js
+++ src/models/CategoriasModel.js
@@ -1,45 +1,34 @@
 const connection = require('./connection');
 
-async function insertCategoryModel() {
+async function insertCategoryModel(name, enable) {
+  await connection.query(`
+    INSERT INTO categories (name, enabled)
+    VALUES('${name}', ${enable})
+  `);
   return;
 }
 
 async function getAllCategories() {
   const getCategories = await connection.query(`
-      SELECT
-              produtos.id,
-              produtos.name,
-              produtos.preco,
-              categorias.name,
-              produtos.descricao
-      FROM produtos
-      JOIN categorias
-      ON produtos.categoria_id = categorias.id
+      select * from categories
   `);
   return getCategories.rows[0];
 }
 
 async function getCategoryById(id) {
   const category = await connection.query(`
-    SELECT
-        produtos.id,
-        produtos.name,
-        produtos.preco,
-        categorias.name,
-        produtos.descricao
-    FROM produtos
-    JOIN categorias
-    ON produtos.categoria_id = categorias.id
-    WHERE produtos.id = ${id}
+    SELECT * FROM categories WHERE id = ${id}
   `);
   return category.rows[0];
 }
 
-async function insertCategoryModel(name, enable) {
-  return;
-}
-
 async function updateCategoryPropertyModel(id, property, newValue) {
+  let value = typeof newValue == String ? `'${newValue}'` : newValue;
+  await connection.query(`
+    UPDATE categories
+    SET ${property} = ${value}
+    WHERE id = 1
+  `);
   return;
 }
 
