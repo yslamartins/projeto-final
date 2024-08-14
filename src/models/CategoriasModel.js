4f9ed6a5b792e78b93eac900a8c9a3e9179e0dfb
= <se_oliveira@outlook.com>
2024-08-13 23:31:19 -0300

    feat: comeco da refatoracao de models

--- src/models/CategoriasModel.js
+++ src/models/CategoriasModel.js
@@ -1,60 +1,53 @@
 const connection = require('./connection');
 
-async function insertCategoryModel(name, slug, useMenu) {
-  await connection.query(`
-    INSERT INTO categories (name, slug, use_in_menu)
-    VALUES('${name}', '${slug}', ${useMenu})
-  `);
-
+async function insertCategoryModel() {
   return;
 }
 
 async function getAllCategories() {
-  const getCategories = await connection.query('SELECT * FROM categories');
-
-  return getCategories.rows;
+  const getCategories = await connection.query(`
+      SELECT
+              produtos.id,
+              produtos.name,
+              produtos.preco,
+              categorias.name,
+              produtos.descricao
+      FROM produtos
+      JOIN categorias
+      ON produtos.categoria_id = categorias.id
+  `);
+  return getCategories.rows[0];
 }
 
 async function getCategoryById(id) {
-  const category = await connection.query(
-    `SELECT * FROM categories WHERE id = ${id}`,
-  );
-
+  const category = await connection.query(`
+    SELECT
+        produtos.id,
+        produtos.name,
+        produtos.preco,
+        categorias.name,
+        produtos.descricao
+    FROM produtos
+    JOIN categorias
+    ON produtos.categoria_id = categorias.id
+    WHERE produtos.id = ${id}
+  `);
   return category.rows[0];
 }
 
 async function insertCategoryModel(name, enable) {
-  await connection.query(`
-    INSERT INTO categories (name, enable)
-    VALUES('${name}', ${enable})
-  `);
-
   return;
 }
 
 async function updateCategoryPropertyModel(id, property, newValue) {
-  await connection.query(`
-    UPDATE categories
-    SET ${property} = '${newValue}'
-    WHERE id = ${id}
-  `);
-
   return;
 }
 
 async function updateEveryPropertyCategoryModel(id, name, enable) {
-  await connection.query(`
-    UPDATE categories
-    SET name = '${name}', enable = ${enable}
-    WHERE id = ${id}
-  `);
-
   return;
 }
 
 async function deleteCategoryModel(id) {
-  await connection.query(`DELETE FROM categories WHERE id = ${id}`);
-
   return;
 }
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/products`);
+  console.log(`Server is running on http://localhost:${port}/products`);
 });
