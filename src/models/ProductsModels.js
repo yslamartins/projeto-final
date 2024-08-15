0d487b0c54eaec36583560e3692d3c162a27a54e
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-08-15 00:03:18 -0300

    refactor: productModel

--- src/models/ProductsModels.js
+++ src/models/ProductsModels.js
@@ -25,7 +25,6 @@ async function getAllProductsModel() {
     SELECT p.*, c.name as category_name 
     FROM products p 
     LEFT JOIN categories c ON p.categorie_id = c.id`;
-    
   const result = await connection.query(query);
   return result.rows;
 }
@@ -36,7 +35,6 @@ async function getProductByIdModel(id) {
     FROM products p 
     LEFT JOIN categories c ON p.categorie_id = c.id 
     WHERE p.id = $1`;
-    
   const result = await connection.query(query, [id]);
   return result.rows[0];
 }
