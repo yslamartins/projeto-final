f0aa71dd12b05d739b16466d5c42404af64b1c7a
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-08-19 16:16:24 -0300

    fix: atualizar swagger de users e produtos

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -8,40 +8,54 @@
  *         name:
  *           type: string
  *           description: Nome do produto
+ *           example: "Camiseta de Algodão"
  *         price:
  *           type: number
  *           format: float
  *           description: Preço do produto
+ *           example: 29.99
  *         discount_percentage:
  *           type: number
  *           format: float
  *           description: Porcentagem de desconto do produto
+ *           example: 10.5
  *         description:
  *           type: string
  *           description: Descrição do produto
+ *           example: "Camiseta de algodão 100% com estampa exclusiva."
  *         stock:
  *           type: integer
  *           description: Quantidade em estoque do produto
+ *           example: 150
  *         category_name:
  *           type: string
  *           description: Nome da categoria associada ao produto
+ *           example: "Roupas"
  *         images:
  *           type: array
  *           items:
  *             $ref: '#/components/schemas/ProductImage'
  *           description: Lista de imagens associadas ao produto
+ *           example:
+ *             - content: "https://example.com/image1.jpg"
+ *               enabled: true
+ *             - content: "https://example.com/image2.jpg"
+ *               enabled: false
  *         enabled:
  *           type: boolean
  *           description: Indica se o produto está habilitado
+ *           example: true
  *     ProductImage:
  *       type: object
  *       properties:
  *         content:
  *           type: string
  *           description: URL ou caminho da imagem do produto
+ *           example: "https://example.com/image1.jpg"
  *         enabled:
  *           type: boolean
  *           description: Indica se a imagem do produto está habilitada
+ *           example: true
  *   securitySchemes:
  *     bearerAuth:
  *       type: http
@@ -73,6 +87,7 @@
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/Product'
+ *       required: true
  *     responses:
  *       '201':
  *         description: Produto criado com sucesso
@@ -100,6 +115,8 @@
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/Product'
+ *       '404':
+ *         description: Produto não encontrado
  *   put:
  *     summary: Atualizar um produto por ID
  *     tags:
@@ -116,6 +133,7 @@
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/Product'
+ *       required: true
  *     responses:
  *       '200':
  *         description: Produto atualizado com sucesso
@@ -123,6 +141,8 @@
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/Product'
+ *       '404':
+ *         description: Produto não encontrado
  *     security:
  *       - bearerAuth: []
  *   delete:
@@ -139,6 +159,8 @@
  *     responses:
  *       '204':
  *         description: Produto deletado com sucesso
+ *       '404':
+ *         description: Produto não encontrado
  *     security:
  *       - bearerAuth: []
  */
--- src/routes/usersRouters.js
+++ src/routes/usersRouters.js
@@ -44,6 +44,13 @@
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/User'
+ *           examples:
+ *             example1:
+ *               value:
+ *                 firstname: John
+ *                 surname: Doe
+ *                 email: john.doe@example.com
+ *                 password: securepassword123
  *     responses:
  *       201:
  *         description: Usuário criado com sucesso
@@ -56,6 +63,8 @@
  *   get:
  *     summary: Obtém todos os usuários
  *     description: Retorna uma lista de todos os usuários.
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       200:
  *         description: Lista de usuários
@@ -79,6 +88,8 @@
  *         required: true
  *         schema:
  *           type: integer
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       200:
  *         description: Detalhes do usuário
@@ -104,6 +115,13 @@
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/User'
+ *           examples:
+ *             example1:
+ *               value:
+ *                 firstname: Jane
+ *                 surname: Doe
+ *                 email: jane.doe@example.com
+ *                 password: newsecurepassword123
  *     responses:
  *       200:
  *         description: Usuário atualizado com sucesso
@@ -124,6 +142,8 @@
  *         required: true
  *         schema:
  *           type: integer
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       204:
  *         description: Usuário deletado com sucesso
@@ -154,6 +174,10 @@
  *               password:
  *                 type: string
  *                 example: newsecurepassword123
+ *           examples:
+ *             example1:
+ *               value:
+ *                 password: newsecurepassword123
  *     responses:
  *       200:
  *         description: Senha atualizada com sucesso
